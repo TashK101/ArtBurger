1808901a26452d9eaea68d0ca4dbed8d0459fa99
TashK101 <Natalia.Karimova@urfu.me>
2023-05-19 20:15:51 +0500

    custom-burger start

---
+++ custom-burger.js
@@ -0,0 +1,75 @@
+window.addEventListener('load', function() {
+    document.querySelector('input[type="file"]').addEventListener('change', function() {
+        if (this.files && this.files[0]) {
+            const base = document.querySelector('.image-base');
+            const img = document.querySelector('.image-overlay');
+            const canvas = document.querySelector('.my-canvas');
+            img.onload = () => {
+                URL.revokeObjectURL(img.src);
+            }
+
+            img.src = URL.createObjectURL(this.files[0]);
+            img.classList.remove('image-overlay');
+            canvas.classList.remove('my-canvas');
+
+            img.classList.add('burger-img');
+            canvas.classList.add('burger-img');
+
+
+            // const fisheye = new Fisheye(canvas);
+            //
+            // fisheye.setViewport(500, 500);
+            // fisheye.setDistortion(10, 10, 10);
+            // fisheye.clear();
+            //
+            // fisheye.draw(img);
+
+        }
+    });
+});
+
+function crop(url, aspectRatio) {
+    // we return a Promise that gets resolved with our canvas element
+    return new Promise((resolve) => {
+        // this image will hold our source image data
+        const inputImage = new Image();
+
+        // we want to wait for our image to load
+        inputImage.onload = () => {
+            // let's store the width and height of our image
+            const inputWidth = inputImage.naturalWidth;
+            const inputHeight = inputImage.naturalHeight;
+
+            // get the aspect ratio of the input image
+            const inputImageAspectRatio = inputWidth / inputHeight;
+
+            // if it's bigger than our target aspect ratio
+            let outputWidth = inputWidth;
+            let outputHeight = inputHeight;
+            if (inputImageAspectRatio > aspectRatio) {
+                outputWidth = inputHeight * aspectRatio;
+            } else if (inputImageAspectRatio < aspectRatio) {
+                outputHeight = inputWidth / aspectRatio;
+            }
+
+            // calculate the position to draw the image at
+            const outputX = (outputWidth - inputWidth) * 0.5;
+            const outputY = (outputHeight - inputHeight) * 0.5;
+
+            // create a canvas that will present the output image
+            const outputImage = document.createElement('canvas');
+
+            // set it to the same size as the image
+            outputImage.width = outputWidth;
+            outputImage.height = outputHeight;
+
+            // draw our image at position 0, 0 on the canvas
+            const ctx = outputImage.getContext('2d');
+            ctx.drawImage(inputImage, outputX, outputY);
+            resolve(outputImage);
+        };
+
+        // start loading our image
+        inputImage.src = url;
+    });
+}
