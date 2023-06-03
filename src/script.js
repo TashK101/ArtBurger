66e6564351f54db60e6ba96158f1f3b2f7e5f64a
Владислав Медведевский <GooDeene>
2023-06-04 01:35:00 +0500

    add preload animation, fixes, new images

--- src/script.js
+++ src/script.js
@@ -1,9 +1,16 @@
 const sidenav = document.querySelector(".sidenav")
 const sidenavOpen = document.querySelector("#openNav");
 const sidenavClose = document.querySelector("#closeNav");
+const reservation = document.querySelector(".reservation");
+const reservationOpen = document.querySelector("#button_booking");
+const reservationClose = document.querySelector(".reservation_closeNav");
+const preloaderScene = document.querySelector('.preloader_scene')
 
 sidenavOpen.addEventListener("click", () => sidenav.style.width = "513px")
 sidenavClose.addEventListener("click", () => sidenav.style.width = "0px")
+
+reservationOpen.addEventListener("click", () => reservation.style.width = "60%")
+reservationClose.addEventListener("click", () => reservation.style.width = "0")
 
 function openMenuPage() {
     window.open("../menu/menu.html");
@@ -12,3 +19,5 @@ function openMenuPage() {
 function openBookingPage() {
     window.open("../booking/booking.html");
 }
+
+setTimeout( () => preloaderScene.style.display = 'none', 5000);
