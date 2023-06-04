5cb64b9776e7683433fd393398d917ae785bda91
Владислав Медведевский <GooDeene>
2023-06-05 02:04:35 +0500

    add new images, button animsations, bug fixes

--- src/script.js
+++ src/script.js
@@ -4,20 +4,22 @@ const sidenavClose = document.querySelector("#closeNav");
 const reservation = document.querySelector(".reservation");
 const reservationOpen = document.querySelector("#button_booking");
 const reservationClose = document.querySelector(".reservation_closeNav");
-const preloaderScene = document.querySelector('.preloader_scene')
+const preloaderScene = document.querySelector('.preloader_scene');
+const reservationLink = document.querySelector('#reservation_link');
 
-sidenavOpen.addEventListener("click", () => sidenav.style.width = "513px")
-sidenavClose.addEventListener("click", () => sidenav.style.width = "0px")
+sidenavOpen.addEventListener("click", () => sidenav.style.width = "513px");
+sidenavClose.addEventListener("click", () => sidenav.style.width = "0px");
 
-reservationOpen.addEventListener("click", () => reservation.style.width = "60%")
-reservationClose.addEventListener("click", () => reservation.style.width = "0")
+reservationOpen.addEventListener("click", () => reservation.style.width = "60%");
+reservationLink.addEventListener("click", () => reservation.style.width = "60%");
+reservationClose.addEventListener("click", () => reservation.style.width = "0");
 
-function openMenuPage() {
+/* function openMenuPage() {
     window.open("../menu/menu.html");
 }
 
 function openBookingPage() {
     window.open("../booking/booking.html");
-}
+} */
 
-setTimeout( () => preloaderScene.style.display = 'none', 5000);
+setTimeout(() => preloaderScene.style.display = 'none', 5000);
